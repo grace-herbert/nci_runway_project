ed26ed9af45abbb7e60259c576b6a304b25610e0
Grace Herbert <herbert.grace.c@gmail.com>
2023-01-07 00:56:55 +0000

    Logout at HomeBtn done on ui pages.

--- app/src/main/java/com/example/runway_project/GetHelp.java
+++ app/src/main/java/com/example/runway_project/GetHelp.java
@@ -30,6 +30,13 @@ public class GetHelp extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //solution to app closing on home button found: https://stackoverflow.com/questions/21901015/how-to-kill-the-application-with-the-home-button
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/runway_project/StayingSafeOnline.java
+++ app/src/main/java/com/example/runway_project/StayingSafeOnline.java
@@ -17,6 +17,13 @@ public class StayingSafeOnline extends AppCompatActivity {
 
     private ActivityStayingSafeOnlineBinding binding;
 
+    //solution to app closing on home button found: https://stackoverflow.com/questions/21901015/how-to-kill-the-application-with-the-home-button
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.finish();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
