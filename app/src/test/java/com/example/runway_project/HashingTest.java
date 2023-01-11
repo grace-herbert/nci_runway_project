aec62544381b7f6f75d0509202a8ddbce6db967c
Grace Herbert <herbert.grace.c@gmail.com>
2023-01-11 10:28:53 +0000

    Clearing hardcoded data in HashingTest

--- app/src/test/java/com/example/runway_project/HashingTest.java
+++ app/src/test/java/com/example/runway_project/HashingTest.java
@@ -10,57 +10,66 @@ public class HashingTest extends TestCase {
     private String hk;
     private String hkC;
 
-    Hashing h = new Hashing("test@test.ie","testthis");
-    Hashing hImg = new Hashing("image2");
-    Hashing allH = new Hashing("test2@test.ie", "testing", "test");
+    private String testEmail1 = "test@test.ie";
+    private String testEmail2 = "test2@test.ie";
+    private String testPwd1 = "testthis";
+    private String testPwd2 = "testing";
+    private String testPwd3 = "test";
+    private String testImg2 = "image";
+    private String testImg1 = "image2";
+
+
+    Hashing h = new Hashing(testEmail1,testPwd1);
+    Hashing hImg = new Hashing(testImg1);
+    Hashing allH = new Hashing(testEmail2, testPwd2, testPwd3);
 
     public void testHashingEmailH(){
-        assertEquals(true, BCrypt.checkpw("test@test.ie", h.getHashEmail()));
+        assertEquals(true, BCrypt.checkpw(testEmail1, h.getHashEmail()));
     }
 
     public void testHashingPwdH(){
-        assertEquals(true, BCrypt.checkpw("testthis", h.getHk()));
+        assertEquals(true, BCrypt.checkpw(testPwd1, h.getHk()));
     }
 
     public void testHashingEmailHExpectFalse(){
-        assertEquals(false, BCrypt.checkpw("testthis", h.getHashEmail()));
+        assertEquals(false, BCrypt.checkpw(testPwd1, h.getHashEmail()));
     }
 
     public void testHashingPwdHExpectFalse(){
-        assertEquals(false, BCrypt.checkpw("test@test.ie", h.getHk()));
+        assertEquals(false, BCrypt.checkpw(testEmail1, h.getHk()));
     }
 
     public void testHashingEmailAllH(){
-        assertEquals(true, BCrypt.checkpw("test2@test.ie", allH.getHashEmail()));
+        assertEquals(true, BCrypt.checkpw(testEmail2, allH.getHashEmail()));
     }
 
     public void testHashingPwdAllH(){
-        assertEquals(true, BCrypt.checkpw("testing", allH.getHk()));
+        assertEquals(true, BCrypt.checkpw(testPwd2, allH.getHk()));
     }
 
     public void testHashingPwdCAllH(){
-        assertEquals(true, BCrypt.checkpw("test", allH.getHkC()));
+        assertEquals(true, BCrypt.checkpw(testPwd3, allH.getHkC()));
     }
 
 
     public void testHashingEmailAllHExpectFalse(){
-        assertEquals(false, BCrypt.checkpw("test", allH.getHashEmail()));
+        assertEquals(false, BCrypt.checkpw(testPwd3, allH.getHashEmail()));
     }
 
     public void testHashingPwdAllHExpectFalse(){
-        assertEquals(false, BCrypt.checkpw("test@test.ie", allH.getHk()));
+        assertEquals(false, BCrypt.checkpw(testEmail1, allH.getHk()));
     }
 
     public void testHashingPwdCAllHExpectFalse(){
-        assertEquals(false, BCrypt.checkpw("test@test.ie", allH.getHkC()));
+        assertEquals(false, BCrypt.checkpw(testEmail1, allH.getHkC()));
     }
 
     public void testHashingHImg(){
-        assertEquals(true, BCrypt.checkpw("image2", hImg.getHashImgName()));
+        assertEquals(true, BCrypt.checkpw(testImg1, hImg.getHashImgName()));
     }
 
     public void testHashingHImgExpectFalse(){
-        assertEquals(false, BCrypt.checkpw("image", hImg.getHashImgName()));
+        assertEquals(false, BCrypt.checkpw(testImg2, hImg.getHashImgName()));
     }
 
 }
